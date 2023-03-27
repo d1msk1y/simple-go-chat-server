dcf8c4aafef06e59aa1a9869e8407ea262ab472d
d1msk1y <yatcenko.dmitriy@gmail.com>
2023-03-27 14:27:36 +0200

    GET command getAlbumByID added

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 type message struct {
+	ID       string `json:"id"`
 	Username string `json:"username"`
 	Time     string `json:"time"`
 	Message  string `json:"message"`
@@ -13,15 +14,16 @@ type message struct {
 
 // test slice of message structs
 var messages = []message{
-	{Username: "d1msk1y 1", Time: "00:00", Message: "Hellow World!"},
-	{Username: "d1msk1y 2", Time: "00:01", Message: "Hellow d1msk1y!"},
-	{Username: "d1msk1y 1", Time: "00:02", Message: "How ya doin?"},
-	{Username: "d1msk1y 2", Time: "00:03", Message: "aight, and u?"},
+	{ID: "0", Username: "d1msk1y 1", Time: "00:00", Message: "Hellow World!"},
+	{ID: "1", Username: "d1msk1y 2", Time: "00:01", Message: "Hellow d1msk1y!"},
+	{ID: "2", Username: "d1msk1y 1", Time: "00:02", Message: "How ya doin?"},
+	{ID: "3", Username: "d1msk1y 2", Time: "00:03", Message: "aight, and u?"},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/messages", getMessages)
+	router.GET("/messages/:id", getAlbumByID)
 	router.POST("/messages", postMessage)
 
 	router.GET("/", func(c *gin.Context) {
@@ -47,4 +49,16 @@ func postMessage(c *gin.Context) {
 
 func getMessages(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, messages)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range messages {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "message not found!"})
 }
